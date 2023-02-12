e980602520e3c420f9827bb4b595f51abd4b26c3
Olena Voitsekhivska <mirkbog@gmail.com>
2023-02-12 13:51:03 +0200

    third line in purple

--- newjs.js
+++ newjs.js
@@ -1,2 +1,4 @@
 console.log('whatever');
 console.log('what the hell');
+
+console.log('wait');
---
+++ purple.js
@@ -0,0 +1,5 @@
+console.log('purple');
+
+console.log('added another line');
+
+console.log('and this one');
